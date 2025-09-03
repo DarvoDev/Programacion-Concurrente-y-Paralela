80a228bcf910f87cba465e4942f15718a7ee125c
DarvoDev <davidcarcamobony@hotmail.com>
2025-09-02 23:19:52 -0600

    E3

---
+++ ManualPracticas1/PCyP_Man1E3.cpp
@@ -0,0 +1,26 @@
+// Ejercicio 3
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int j = 0, k = 0;
+
+int main(){
+    for(int i = 1; i <= 3; i++){ 
+        
+        //Uso de funciones lambda
+        thread hilo1([](){
+            j = j + 10;
+        });
+
+        thread hilo2([](){
+            k = k + 100;
+        });
+
+        hilo1.join();
+        hilo2.join();
+    }
+
+    return 0;
+}
